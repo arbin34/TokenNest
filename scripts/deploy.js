8c976ed42d8adee02539b0c874634cdbfcb1a7a4
verman <99386958+arbin34@users.noreply.github.com>
2025-10-29 00:03:25 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,16 @@
 
+const hre = require("hardhat");
+
+async function main() {
+  const Contract = await hre.ethers.getContractFactory("TokenNest");
+  const instance = await Contract.deploy();
+  await instance.deployed();
+
+  console.log("TokenNest deployed to:", instance.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
