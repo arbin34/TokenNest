e7bce15c3ebbc887a61d1f8325dfecd4501775e4
verman <99386958+arbin34@users.noreply.github.com>
2025-11-03 17:36:50 +0530

    Update TokenNet.sol

--- contracts/TokenNet.sol
+++ contracts/TokenNet.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.17;
 /**
  * @title TokenNest
  * @notice Minimal and gas-efficient smart contract for demonstration.
+ * describe anout the project .
  */
 
 contract TokenNest {
